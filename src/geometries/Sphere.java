e921fe0914fc56f9dc2a6be867bca4120ea469a2
Kaduri Yosef <kaduri184@gmail.com>
2024-06-04 17:32:34 +0300

    finish the test of sphere

--- src/geometries/Sphere.java
+++ src/geometries/Sphere.java
@@ -36,7 +36,9 @@ public class Sphere extends RadialGeometry {
 
     @Override
     public List<Point> findIntersections(Ray ray) {
-
+        if (center.equals(ray.getHead())) {
+            return List.of(ray.getPoint(radius));
+        }
         Vector u = center.subtract(ray.getHead());
         double tm = u.dotProduct(ray.getDir());
         double dSquared = u.lengthSquared() - tm * tm;
